614264f6b5c4efa79189f7a6db8441270c165953
Francesco Delacqua <bi0nd0@users.noreply.github.com>
2024-07-16 00:22:58 -0500

    bugfix

--- aou_utils/SurveyQueryBuilder.py
+++ aou_utils/SurveyQueryBuilder.py
@@ -10,33 +10,33 @@ class SurveyQueryBuilder:
     def get_concept_query(self, concept_ids):
         concept_ids_str = Utils.list_to_string(concept_ids, quote=False)
         return f"""SELECT
-                        criteria.person_id 
+                    criteria.person_id 
+                FROM
+                    (SELECT
+                        DISTINCT person_id, entry_date, concept_id 
                     FROM
-                        (SELECT
-                            DISTINCT person_id, entry_date, concept_id 
+                        `{self.dataset}.cb_search_all_events` 
+                    WHERE
+                        (concept_id IN(SELECT
+                            DISTINCT c.concept_id 
                         FROM
-                            `{self.dataset}.cb_search_all_events` 
-                        WHERE
-                            (concept_id IN(SELECT
-                                DISTINCT c.concept_id 
+                            `{self.dataset}.cb_criteria` c 
+                        JOIN
+                            (SELECT
+                                CAST(cr.id as string) AS id       
                             FROM
-                                `{self.dataset}.cb_criteria` c 
-                            JOIN
-                                (SELECT
-                                    CAST(cr.id as string) AS id       
-                                FROM
-                                    `{self.dataset}.cb_criteria` cr       
-                                WHERE
-                                    concept_id IN ({concept_ids_str})       
-                                    AND full_text LIKE '%_rank1]%'      ) a 
-                                    ON (c.path LIKE CONCAT('%.', a.id, '.%') 
-                                    OR c.path LIKE CONCAT('%.', a.id) 
-                                    OR c.path LIKE CONCAT(a.id, '.%') 
-                                    OR c.path = a.id) 
+                                `{self.dataset}.cb_criteria` cr       
                             WHERE
-                                is_standard = 0 
-                                AND is_selectable = 1) 
-                            AND is_standard = 0 )) criteria"""
+                                concept_id IN (1585873, 1585860)       
+                                AND full_text LIKE '%_rank1]%'      ) a 
+                                ON (c.path LIKE CONCAT('%.', a.id, '.%') 
+                                OR c.path LIKE CONCAT('%.', a.id) 
+                                OR c.path LIKE CONCAT(a.id, '.%') 
+                                OR c.path = a.id) 
+                        WHERE
+                            is_standard = 0 
+                            AND is_selectable = 1) 
+                        AND is_standard = 0 )) criteria"""
     
     def include_concept_ids(self, concept_ids):
         concept_query = self.get_concept_query(concept_ids)
@@ -87,17 +87,8 @@ class SurveyQueryBuilder:
                         answer.PERSON_ID IN (SELECT
                             distinct person_id  
                         FROM
-                            `{self.dataset}.cb_search_person` cb_search_person
-                        WHERE
-                            cb_search_person.person_id IN (SELECT
-                                criteria.person_id
-                            FROM
-                                (SELECT
-                                    DISTINCT person_id, entry_date, concept_id
-                                FROM
-                                    `{self.dataset}.cb_search_all_events`
-                                WHERE {condition}
-                    )"""
+                            `{self.dataset}.cb_search_person` cb_search_person  
+                        WHERE ({condition})"""
         self.selectQuery = getQuery
         return self
         
