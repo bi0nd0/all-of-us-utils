18b39ce1e73a1285a2495fd178081c1efd94e9ca
Francesco Delacqua <bi0nd0@users.noreply.github.com>
2024-07-17 22:13:56 -0500

    modified age calculator

--- aou_utils/AgeCalculator.py
+++ aou_utils/AgeCalculator.py
@@ -3,17 +3,9 @@ import pandas as pd
 class AgeCalculator:
     @staticmethod
     def calculate_age(df, current_date, dob_key="date_of_birth", age_key="age"):
-        def calculate_single_age(birthdate):
-            age = current_date.year - birthdate.year - ((current_date.month, current_date.day) < (birthdate.month, birthdate.day))
-            return age
-
         # Create a copy of the DataFrame to avoid modifying the original
+        today = pd.to_datetime('today')
         df_copy = df.copy()
-
-        # Ensure the date_of_birth column in the copied DataFrame is a datetime type
-        df_copy[dob_key] = pd.to_datetime(df_copy[dob_key], errors='coerce')
-
-        # Apply the age calculation to each row in the copied DataFrame
-        df_copy[age_key] = df_copy[dob_key].apply(calculate_single_age)
+        df_copy['age'] = df_copy[dob_key].apply(lambda dob: today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day)))
 
         return df_copy
--- tests/test-survey.py
+++ tests/test-survey.py
@@ -16,7 +16,7 @@ dataset = 'your_dataset_name'  # Replace with your actual dataset name
 qb = SurveyQueryBuilder(dataset)
 
 # Build a query
-query = qb.select().include_concept_ids([1585873, 1585860, 1234]).exclude_persons([1,2,3,4,5,6,7,8,9,0]).build()
+query = qb.select().include_concept_ids([1585873, 1585860]).exclude_persons([1,2,3,4,5,6,7,8,9,0]).build()
 
 # Display the first 5 rows of the DataFrame
 print(query)
