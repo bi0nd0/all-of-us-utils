317940d60bf6f7e2232dbda193d8096ae36b8d90
Francesco Delacqua <bi0nd0@users.noreply.github.com>
2024-07-21 11:19:23 -0500

    tweak

--- aou_utils/AgeCalculator.py
+++ aou_utils/AgeCalculator.py
@@ -2,7 +2,7 @@ from typing import Optional
 import pandas as pd
 
 class AgeCalculator:
-    def __init__(self, current_date: Optional[str] = None, dob_key: str = "birth_datetime", age_key: str = "age"):
+    def __init__(self, current_date: Optional[str] = None, dob_key: str = "date_of_birth", age_key: str = "age"):
         self.current_date = pd.to_datetime(current_date) if current_date else pd.to_datetime('today')
         self.dob_key = dob_key
         self.age_key = age_key
