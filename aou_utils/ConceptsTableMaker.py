7885c71328fd0559a14c1a3fcdcf0b67a7c5d197
Francesco Delacqua <bi0nd0@users.noreply.github.com>
2024-08-25 16:55:12 -0700

    fixes

--- aou_utils/ConceptsTableMaker.py
+++ aou_utils/ConceptsTableMaker.py
@@ -6,34 +6,35 @@ class ConceptsTableMaker:
     def __init__(self):
         pass
     
-    def combine_concepts_counts(self, dataset, person_ids, concepts_group):
+    def combine_concepts_counts(self, dataset, person_ids, concept_group):
         """
-        Combines the counts of a single comorbidity into a DataFrame.
+        Combines the counts of a single comorbidity into a DataFrame and returns the 
+        results along with the detailed DataFrame of persons with the condition.
         
         Parameters:
         - dataset: The dataset to be used for querying.
         - person_ids: The list of person IDs to be considered.
-        - concepts_group: A list containing one dictionary with 'label', 'ids', and 'code' for a comorbidity.
+        - concept_group: A dictionary with 'label', 'ids', and 'code' for a comorbidity.
         
         Returns:
-        - A DataFrame with the total count and percentage of persons with the given comorbidity.
+        - result_df: A DataFrame with the total count and percentage of persons with the given comorbidity.
+        - df: A DataFrame of persons who have the given comorbidity.
         """
         data = []
         total_persons = len(person_ids)
         qb = QueryBuilder(dataset)
-        qb = qb.selectDemography()
         
-        for entry in concepts_group:
-            label = entry['label']
-            concept_ids = entry['ids']
-            query = qb.include_concept_ids(concept_ids).include_persons(person_ids).build()
-            df = Utils.get_dataframe(query)
-            total = len(df)  # Get the count of the results
-            percentage = (total / total_persons) * 100 if total_persons > 0 else 0
-            data.append({'label': label, 'total': total, 'percentage': percentage})
+        qb = qb.selectDemography()
+        label = concept_group['label']
+        concept_ids = concept_group['ids']
+        query = qb.include_concept_ids(concept_ids).include_persons(person_ids).build()
+        df = Utils.get_dataframe(query)
+        total = len(df)  # Get the count of the results
+        percentage = (total / total_persons) * 100 if total_persons > 0 else 0
+        data.append({'label': label, 'total': total, 'percentage': percentage})
 
         result_df = pd.DataFrame(data)
-        return result_df
+        return result_df, df
 
     def combine_all_comorbidities_counts(self, dataset, person_ids, comorbidities):
         """
