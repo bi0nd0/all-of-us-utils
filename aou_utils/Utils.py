9b34308e0c2ea52c8795e968b7e434d53a56b4e2
Francesco Delacqua <bi0nd0@users.noreply.github.com>
2024-08-31 15:48:40 -0500

    count columns

--- aou_utils/Utils.py
+++ aou_utils/Utils.py
@@ -1,4 +1,5 @@
 import re, pandas as pd, os
+import numpy as np
 class Utils:
     @staticmethod
     def list_to_string(items, quote=False):
@@ -47,3 +48,44 @@ class Utils:
         df_copy[age_key] = df_copy[dob_key].apply(calculate_single_age)
 
         return df_copy
+
+    @staticmethod
+    def count_column_values(df, total_participants, count_column, label_column=None):
+        """
+        Counts the total number of occurrences of each unique value in count_column, optionally labeling the counts 
+        with the corresponding values from label_column. Also calculates the percentage of each count based on the 
+        provided total number of participants. The percentage is reported fully in one column and truncated to two 
+        decimal places in the formatted column.
+
+        Parameters:
+        - df (pd.DataFrame): The input DataFrame.
+        - total_participants (int): The total number of participants to calculate percentages.
+        - count_column (str): The column name in df to count the occurrences of.
+        - label_column (str, optional): An optional column name in df to use as labels for the counts.
+        
+        Returns:
+        - pd.DataFrame: A DataFrame with four columns: 'label', 'total', 'percentage', and 'formatted_total_percentage'.
+        'label' contains either the values from label_column (if provided) or count_column, 'total' contains the corresponding counts,
+        'percentage' contains the full percentages, and 'formatted_total_percentage' contains the truncated 
+        'total (percentage)' strings.
+        """
+        if label_column:
+            # Group by count_column and label_column, count occurrences, and reset index
+            result_df = df.groupby([count_column, label_column]).size().reset_index(name='total')
+            # Use label_column as the label column
+            result_df = result_df[[label_column, 'total']].rename(columns={label_column: 'label'})
+        else:
+            # Count occurrences of each unique value in count_column
+            result_df = df[count_column].value_counts().reset_index()
+            result_df.columns = ['label', 'total']
+        
+        # Calculate percentage using the total_participants provided
+        result_df['percentage'] = (result_df['total'] / total_participants) * 100
+        
+        # Truncate the percentage for formatting, without altering the original percentage
+        truncated_percentages = result_df['percentage'].apply(lambda x: np.floor(x * 100) / 100.0)
+        
+        # Create the formatted 'total (percentage)' column with truncated percentages
+        result_df['formatted_total_percentage'] = result_df['total'].astype(str) + ' (' + truncated_percentages.round(2).astype(str) + '%)'
+        
+        return result_df
